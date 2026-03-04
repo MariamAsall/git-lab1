e04542615b6e515b06996e4a2c165eaa2a1bbd22
MariamAsall <mariamasal075@gmail.com>
2026-03-04 12:05:08 +0200

    Add printHelloGit method to HelloITI class

---
+++ HelloITI.java
@@ -0,0 +1,11 @@
+public class HelloITI {
+    
+    public void printHelloITI() {
+        System.out.println("Hello ITI");
+    }
+    public void printHelloGit() {
+        System.out.println("Hello Git");
+    }
+}
+
+
